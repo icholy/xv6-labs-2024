5f9cabc8c93959390258f373de49cdccd59d3ae4
Ilia Choly <ilia.choly@gmail.com>
2025-06-09 09:10:48 -0400

    keep searching after match

--- user/attack.c
+++ user/attack.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[]) {
   for (int off = 0; off < sz ; off++) {
     if (memcmp(end+off, "my very very very secret pw is:", 31) == 0) {
       fprintf(2, "OK: secret is '%s'\n", end+off+32);
-      exit(0);
     }
   }
   exit(0);
