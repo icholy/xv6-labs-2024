0d264a00256b7a114212d78bf5135fb718bc8788
Ilia Choly <ilia.choly@gmail.com>
2025-06-09 09:03:10 -0400

    use memcpm instead of strcmp to ignore null terminators

--- user/attack.c
+++ user/attack.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) {
   char *end = sbrk(sz);
 
   for (int off = 0; off < sz ; off++) {
-    if (strcmp(end+off, "my very very very secret pw is:") == 0) {
-      fprintf(2, "OK: secret is %s", end+off+32);
+    if (memcmp(end+off, "my very very very secret pw is:", 31) == 0) {
+      fprintf(2, "OK: secret is '%s'\n", end+off+32);
       exit(0);
     }
   }
