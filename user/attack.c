866bf493267f25308e562945b879f76949a3549e
Ilia Choly <ilia.choly@gmail.com>
2025-06-09 14:21:12 -0400

    syscall lab finished

--- user/attack.c
+++ user/attack.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     char *end = sbrk(PGSIZE);
     for (int off = 0; off < PGSIZE - 40; off++) {
       if (memcmp(end + off, "secret pw is:", 12) == 0) {
-        fprintf(2, "OK: secret is %s\n", end + off + 14);
+        fprintf(2, "%s", end + off + 14);
         exit(0);
       }
     }
