41684ff292eddffee54980b1f32db22ec91edcf3
Ilia Choly <ilia.choly@gmail.com>
2025-06-09 08:15:47 -0400

    add notes to secret.c

--- user/secret.c
+++ user/secret.c
@@ -11,8 +11,11 @@ main(int argc, char *argv[])
     printf("Usage: secret the-secret\n");
     exit(1);
   }
+  // grow heap by 32 pages
   char *end = sbrk(PGSIZE*32);
+  // point 9 pages into the heap
   end = end + 9 * PGSIZE;
+  // write secret into the page 9
   strcpy(end, "my very very very secret pw is:   ");
   strcpy(end+32, argv[1]);
   exit(0);
