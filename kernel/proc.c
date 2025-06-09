94b46495a9a34f9492af38c384b524122db932f6
Ilia Choly <ilia.choly@gmail.com>
2025-06-09 07:45:55 -0400

    add trace syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -169,6 +169,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace = 0;
 }
 
 // Create a user page table for a given process, with no user memory,
@@ -321,6 +322,8 @@ fork(void)
   acquire(&np->lock);
   np->state = RUNNABLE;
   release(&np->lock);
+
+  np->trace = p->trace;
 
   return pid;
 }
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void)
+{
+  int mask;
+
+  argint(0, &mask);
+
+  // does this need locking?
+  myproc()->trace = mask;
+  return 0;
+}
