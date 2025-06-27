d5a1eb880d8873fc353f854a4f713edf3ec551d2
Ilia Choly <ilia.choly@gmail.com>
2025-06-27 17:12:08 -0400

    use continue

--- kernel/vm.c
+++ kernel/vm.c
@@ -489,12 +489,13 @@ vmprint(pagetable_t pagetable) {
   printf("page table %p\n", pagetable);
   for (int l2 = 0; l2 < 512; l2++) {
     pte_t pte2 = pagetable[l2];
-    if (pte2 & PTE_V) {
-      printf("..%p: pte %p pa %p\n",
-        (void *)(uint64)((l2 & PXMASK) << PXSHIFT(2)),
-        (void *)(uint64)pte2,
-        (void *)PTE2PA(pte2));
+    if ((pte2 & PTE_V) == 0) {
+      continue;
     }
+    printf("..%p: pte %p pa %p\n",
+      (void *)(uint64)((l2 & PXMASK) << PXSHIFT(2)),
+      (void *)(uint64)pte2,
+      (void *)PTE2PA(pte2));
   }
 }
 #endif
