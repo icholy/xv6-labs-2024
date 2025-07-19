271c2108afb83217ab955ddcc0559e55817d668e
Ilia Choly <ilia.choly@gmail.com>
2025-07-19 11:15:20 -0400

    add r_s0

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -338,6 +338,14 @@ r_ra()
   return x;
 }
 
+static inline uint64
+r_s0()
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x) );
+  return x;
+}
+
 // flush the TLB.
 static inline void
 sfence_vma()
